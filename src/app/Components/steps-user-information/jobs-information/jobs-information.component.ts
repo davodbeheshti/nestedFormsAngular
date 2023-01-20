6b4a10e4a979c91a5e1f5fb0a63e2683881f34a7
davodbeheshti <davood.beheshti2018@gmail.copm>
2023-01-20 10:55:22 -0800

    adding base components

---
+++ src/app/Components/steps-user-information/jobs-information/jobs-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jobs-information',
+  templateUrl: './jobs-information.component.html',
+  styleUrls: ['./jobs-information.component.scss']
+})
+export class JobsInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
