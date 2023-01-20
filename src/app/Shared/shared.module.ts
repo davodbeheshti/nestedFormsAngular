c481e3642785ea16472847a9c7f8a2518a5a236c
davodbeheshti <davood.beheshti2018@gmail.com>
2023-01-20 12:37:54 -0800

    changes section steps

--- src/app/Shared/shared.module.ts
+++ src/app/Shared/shared.module.ts
@@ -1,14 +1,23 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// importing packages material
+import { MatTabsModule } from '@angular/material/tabs';
 
+
+// packages material
+const materialPackages = [MatTabsModule]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatTabsModule
   ],
-  exports : []
+  exports: [MatTabsModule]
 })
 
 export class SharedModule { }
