2ebcf0c1da4623a72f3090963f1a3ed5edac235a
davodbeheshti <davood.beheshti2018@gmail.com>
2023-02-01 13:08:51 -0800

    fixes section countryInformation

--- src/app/Components/steps-user-information/country-information/country-information.component.ts
+++ src/app/Components/steps-user-information/country-information/country-information.component.ts
@@ -1,4 +1,7 @@
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { ICountry } from 'src/app/Model/ICountry';
+import { ICity } from 'src/app/Model/ICity';
 
 @Component({
   selector: 'app-country-information',
@@ -9,7 +12,31 @@ export class CountryInformationComponent implements OnInit {
 
   constructor() { }
 
+  countries: ICountry[] = []
+  cities: ICity[] = []
+
+  form: FormGroup;
+
   ngOnInit(): void {
+    this.form = new FormGroup({
+      selectCountry: new FormControl(),
+      selectCity: new FormControl(),
+      address: new FormControl(),
+    })
+
+    this.countries = [
+      { name: "Iran", id: '1' }
+    ]
+
+    this.cities = [
+      { name: "Mashhad", id: '1' }
+    ]
+
+
+  }
+
+  test() {
+    console.log(this.form.value);
   }
 
 }
